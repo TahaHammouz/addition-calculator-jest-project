9809d1f5973b0f0718df06376bc308ee7c19c876
Taha Hammouz <tahahammouz2k01@gmail.com>
2022-12-12 20:33:30 +0200

    negatives not allowed

--- additionCalculator.js
+++ additionCalculator.js
@@ -1,17 +1,35 @@
-function additionCalculator(...numbers) {
-  var arg = numbers;
+function additionCalculator(...stringNumbers) {
+  var arg = stringNumbers;
   var argLength = arg.length;
+
+  console.log("array length : " + argLength);
   switch (argLength) {
-    case 0:
-      return "0";
     case 1:
-      return arg[0];
+      let arg1 = parseInt(arg[0]);
+      if (arg[0] == "" && arg[0] == 0) return "0";
+      if (arg1 < 0) {
+        throw new Error("negatives not allowed,you entered :" + arg1);
+      }
+      if (arg1 >= 0) {
+        return arg1;
+      }
+
     case 2:
-      return arg[0] + arg[1];
+      let firstArg = parseInt(arg[0]);
+      let secondArg = parseInt(arg[1]);
+
+      if (firstArg >= 0 && secondArg >= 0) return firstArg + secondArg;
+      else if (firstArg < 0 && secondArg < 0)
+        throw new Error(
+          "negatives not allowed,you entered :" + firstArg + "," + secondArg
+        );
+      else if (firstArg < 0 && secondArg > 0) {
+        throw new Error("negatives not allowed,you entered :" + firstArg);
+      } else if (firstArg > 0 && secondArg < 0) {
+        throw new Error("negatives not allowed,you entered :" + secondArg);
+      }
     default:
       return "There is a problem with the arguments, please try again";
   }
 }
-additionCalculator(1, 4, 7);
-
 module.exports = additionCalculator;
--- additionCalculator.test.js
+++ additionCalculator.test.js
@@ -2,21 +2,55 @@ const additionCalculator = require("./additionCalculator");
 
 describe("additionCalculator's test cases", () => {
   test("empty arguments it will return 0", () => {
-    expect(additionCalculator()).toBe("0");
+    let empty = "";
+    expect(additionCalculator(empty)).toBe("0");
+  });
+  test("one number entered", () => {
+    expect(additionCalculator(1)).toBe(1);
+  });
+  test("one negative number entered", () => {
+    let num1 = String(-1);
+    try {
+      additionCalculator(num1);
+    } catch (error) {
+      expect(error.message).toBe("negatives not allowed,you entered :" + num1);
+    }
   });
 
-  test("addition of one number is the same", () => {
-    expect(additionCalculator(1)).toBe(1);
+  test("both numbers are negative", () => {
+    let num1 = String(-1);
+    let num2 = String(-2);
+    try {
+      additionCalculator(num1, num2);
+    } catch (error) {
+      expect(error.message).toBe(
+        "negatives not allowed,you entered :" + num1 + "," + num2
+      );
+    }
+  });
+  test("first number is negative", () => {
+    let num1 = String(-1);
+    let num2 = String(2);
+    try {
+      additionCalculator(num1, num2);
+    } catch (error) {
+      expect(error.message).toBe("negatives not allowed,you entered :" + num1);
+    }
+  });
+  test("second number is negative", () => {
+    let num1 = String(1);
+    let num2 = String(-2);
+    try {
+      additionCalculator(num1, num2);
+    } catch (error) {
+      expect(error.message).toBe("negatives not allowed,you entered :" + num2);
+    }
   });
 
   test("addition of one plus two is three", () => {
-    expect(additionCalculator(1, 2)).toBe(3);
-  });
-
-  test("addiction support up to 2 numbers only", () => {
-    expect("The function only support up to 2 numbers only.").toBe(
-      "The function only support up to 2 numbers only."
-    );
+    let one = String(1);
+    let two = String(2);
+    expect(additionCalculator(one, two)).toBe(parseInt(one) + parseInt(two));
   });
   test("unknown amount of numbers", () => {
     let str = String(1);
